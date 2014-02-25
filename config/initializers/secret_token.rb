322b8a2091b2f8574e079607ca33d53f7f035134
Saúl Gómez <ing.saul.gomez18@gmail.com>
2014-02-25 11:16:04 -0600

    Online resume app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resume::Application.config.secret_token = '6b597cb958281d0daf5030066ff01a3de196fff36a519eb96f1fa86316dbc8103e3b6631e54f8367bf0e8bf91322e64c07040d68b9af331c9ca172ace7f1d02d'
