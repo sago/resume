fabfcdf771670f6ae211373f098078f07f1ce3ca
Saúl Gómez <ing.saul.gomez18@gmail.com>
2014-03-08 13:33:39 -0600

    Blog Modal Styles, google maps add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,19 @@
 //= require twitter/bootstrap
 //= require_tree .
 $(document).ready(function () {
-    $('#myTab a').click(function (e) {
+  // Tabs function
+  $('#myTab a').click(function (e) {
         e.preventDefault();
         $(this).tab('show');
-    });
-
+  });
+  // Blog Modal
   $('a.external').on('click', function(e) {
         e.preventDefault();
         var url = $(this).attr('href');
         $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="http://www.therubydummy.blogspot.mx"></iframe>');
 
-    });
-    $('#myModal').on('show.bs.modal', function () {
-
+  });
+  $('#myModal').on('show.bs.modal', function () {
         $(this).find('.modal-dialog').css({
                   width:'40%x', //choose your width
                   height:'100%',
@@ -44,5 +44,17 @@ $(document).ready(function () {
                   height:'100%',
                   'padding':'0'
            });
-    });
+  });
+  // Google Maps
+  var map;
+  function initialize() {
+        var mapOptions = {
+            zoom: 10,
+            center: new google.maps.LatLng(19.282093, -103.725880)
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 });
