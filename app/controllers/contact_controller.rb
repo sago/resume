7d82eef3282c8162d8a64d8ece07661b4825b83c
Saúl Gómez <ing.saul.gomez18@gmail.com>
2014-03-09 01:49:36 -0600

    Css Styles for Contact form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -10,7 +10,8 @@ class ContactController < ApplicationController
       redirect_to(root_path, :notice => "Message was successfully sent.")
     else
       flash.now.alert = "Please fill all fields."
-      render :new
+      binding.pry
+      render_partial "contact/new"
     end
   end
 end
